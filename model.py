7353cbfd104dd9cc049815889bce2a361e9d8dd0
Hackbright Student <no-reply@hackbrightacademy.com>
2017-08-01 18:03:33 -0700

    WIP in part 2, stopped at /register

--- model.py
+++ model.py
@@ -49,6 +49,7 @@ class Movie(db.Model):
         return "<Movie movie_id=%s title=%s>" % (self.movie_id,
                                                  self.title)
 
+
 class Rating(db.Model):
     """Ratings given by users for movies in the database"""
 
@@ -58,6 +59,15 @@ class Rating(db.Model):
     movie_id = db.Column(db.Integer, db.ForeignKey('movies.movie_id'))
     user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
     score = db.Column(db.Integer)
+
+    # Define relationship to user
+    user = db.relationship("User",
+                           backref=db.backref("ratings",
+                                              order_by=rating_id))
+    # Define relationship to movie
+    movie = db.relationship("Movie",
+                            backref=db.backref("ratings",
+                                               order_by=rating_id))
 
     def __repr__(self):
         """Provide helpful representation when printed."""
--- server.py
+++ server.py
@@ -2,10 +2,12 @@
 
 from jinja2 import StrictUndefined
 
-from flask import Flask, jsonify
+from flask import (Flask, jsonify, render_template, redirect, request, flash,
+                   session)
+
 from flask_debugtoolbar import DebugToolbarExtension
 
-from model import connect_to_db, db
+from model import User, Rating, Movie, connect_to_db, db
 
 
 app = Flask(__name__)
@@ -23,7 +25,20 @@ app.jinja_env.undefined = StrictUndefined
 def index():
     """Homepage."""
     a = jsonify([1,3])
-    return a
+    return render_template('homepage.html')
+
+@app.route('/register', methods=['GET'])
+def register_form():
+    """Show the login page, with a link to the registration form page."""
+
+    return render_template('users.html')
+
+# @app.route('/register', methods=['POST'])
+# def register_form():
+#     """Show the login page, with a link to the registration form page."""
+
+
+#     return redirect("/")   
 
 
 if __name__ == "__main__":
